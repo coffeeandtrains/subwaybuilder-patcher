d5f16a686c8b535a7da869c1f2b0d4b725e6cc5c
Murphy <murphy@coffeeandtrains.de>
2025-11-10 09:46:06 +0100

    Update download_tiles.js
    Skip map tile download if the map tile was already downloaded and placed into the map_tiles folder

--- scripts/download_tiles.js
+++ scripts/download_tiles.js
@@ -3,10 +3,15 @@ import config from '../config.js';
 
 console.log("Downloading map tiles");
 for(var place of config.places) {
+    if (fs.existsSync(`./map_tiles/${place.code}.pmtiles`)) {
+        console.log(`Skipping ${place.name} (${place.code}) — already downloaded`);
+        continue;
+    }
     console.log(`Fetching tiles for ${place.name} (${place.code})`);
     if(config.platform === 'windows')
         execSync(`${import.meta.dirname}\\..\\map_tiles\\pmtiles.exe extract ${config['protomaps-bucket']} --maxzoom=${config['tile-zoom-level']} --bbox="${place.bbox.join(',')}" ./map_tiles/${place.code}.pmtiles`);
     else if(config.platform === 'linux' || config.platform === 'macos') 
         execSync(`${import.meta.dirname}/../map_tiles/pmtiles extract ${config['protomaps-bucket']} --maxzoom=${config['tile-zoom-level']} --bbox="${place.bbox.join(',')}" ./map_tiles/${place.code}.pmtiles`);
     
+
 }
